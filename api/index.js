f736e3523ac2b7bc134e29f5d35c05ffd86ca06e
Waldemar Glaz <waldekglaz@gmail.com>
2024-04-20 15:43:03 +0200

    connect to the databse

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
 
 const app = express();
 
